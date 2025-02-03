14bdc46d1c971b3a59d9f4ebc2bb2918af37baf4
TikhonP <tikhon.petrishchev@gmail.com>
2025-02-03 22:35:31 +0300

    Bumped version 2.2.2 -> 2.2.3

--- src/speechkit/__init__.py
+++ src/speechkit/__init__.py
@@ -4,7 +4,7 @@ Python SDK for using Yandex Speech recognition and synthesis.
 """
 
 __author__ = 'Tikhon Petrishchev'
-__version__ = '2.2.2'
+__version__ = '2.2.3'
 
 from speechkit._auth import Session
 from speechkit._recognition.streaming_recognition import DataStreamingRecognition
